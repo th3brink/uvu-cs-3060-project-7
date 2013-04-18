e49561d8fc6e5587577ed18c561d542273655642
Bryson <Murraybryson786@yahoo.com>
2013-04-18 10:34:02 -0600

    Part A finished, Replaced Makefile
    Currently parts B, C, and D are just copies of partA.
    So i could test the new Makefile  to make sure it compiles all 4 parts.
    also the executables have also been included for each part (result of
    testing that the Makefile worked for all 4 files)

--- Makefile
+++ Makefile
@@ -1,11 +1,9 @@
+all: parta partb partc partd
 parta : parta.c
-	gcc -g -Wall parta.c -o parta
-    
+	gcc -g -Wall -pthread parta.c -o parta
 partb : partb.c
-	gcc -g -Wall partb.c -o partb
-    
+	gcc -g -Wall -pthread partb.c -o partb
 partc : partc.c
-	gcc -g -Wall partc.c -o partc
-    
+	gcc -g -Wall -pthread partc.c -o partc
 partd : partd.c
-	gcc -g -Wall partd.c -o partd
+	gcc -g -Wall -pthread partd.c -o partd
--- parta.c
+++ parta.c
@@ -1,23 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<pthread.h>
 
-void* incrementCounter( void* m );
+
+int count; 
+
+void* incrementCounter(void*);
 
 
-int count;
-    
-int main () {
-    count = 0;
-    
-    incrementCounter(count);
-    
-    fprint("count: %d", count);
-    
-    
-    return 0;
+int main ( ) {
+	
+	printf("Students:\n");
+	printf("\t-James Brinkerhoff - 10621032\n");
+	printf("\t-Bryson Murray - 10501259\n");
+	printf("\t-Kory Kehl - 1043993\n");
+	printf("Class CS 3060 - Project 5\n");
+
+	pthread_t thread1;
+	pthread_create(&thread1, NULL, &incrementCounter,NULL);
+        pthread_t thread2;
+	pthread_create(&thread2, NULL, &incrementCounter,NULL);
+        pthread_t thread3;
+	pthread_create(&thread3, NULL, &incrementCounter,NULL);
+        pthread_t thread4;
+	pthread_create(&thread4, NULL, &incrementCounter,NULL);
+	printf("count: %d\n ", count); 
+	return 0;
 }
 
 void* incrementCounter( void* m )
-    {
-        int i;
+{
+	int i;
         for (i = 0; i < 10; ++i)
         {
             int tempValue = count;
@@ -25,6 +38,7 @@ void* incrementCounter( void* m )
             tempValue = tempValue + 1;
             count = tempValue;
         }
- 
         return NULL;
-    }
+}
+
+
